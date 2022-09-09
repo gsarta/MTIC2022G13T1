95c156809c924fddfd5664c8caae970d27ada1d2
BrandonSolano12 <BrandonSolano12@users.noreply.github.com>
2022-09-08 21:22:01 -0500

    primer comit

---
+++ MascotaFeliz.App.Dominio/Entidades/Dueno.cs
@@ -0,0 +1,8 @@
+using System;
+namespace MascotaFeliz.App.Dominio
+{
+    public class Dueno:Persona // Similar al extends en Java para heredar atributos
+    {
+        public string Correo {set;get;}
+    }
+}
